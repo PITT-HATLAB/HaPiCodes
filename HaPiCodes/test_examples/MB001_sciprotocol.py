e5cf416d4407ead6c8cea9ad0a2345a8695b216c
borismesits <bmesits.us@gmail.com>
2022-08-22 13:42:28 -0400

    Update MB001_sciprotocol.py

--- HaPiCodes/test_examples/MB001_sciprotocol.py
+++ HaPiCodes/test_examples/MB001_sciprotocol.py
@@ -5,6 +5,7 @@ import h5py
 from HaPiCodes.test_examples import basicMsmtPulses as bmp
 from HaPiCodes.data_process import fittingAndDataProcess as f
 from HaPiCodes.pathwave.pxi_instruments import PXI_Instruments
+from HaPiCodes.data_process import postSelectionProcess as psdp
 from HaPiCodes.test_examples import msmtInfoSel
 
 def sciprotocol(yamlFile=msmtInfoSel.cwYaml, ampArray=np.linspace(0, 0.9, 100), plot=1, update=0):
@@ -20,8 +21,36 @@ def sciprotocol(yamlFile=msmtInfoSel.cwYaml, ampArray=np.linspace(0, 0.9, 100), 
     IQdata = f.processDataReceiveWithRef(pxi.subbuffer_used, dataReceive, plot=1)
     Id, Qd = f.average_data(IQdata.I_rot, IQdata.Q_rot)
     piPulseAmp = f.pi_pulse_tune_up(Id, Qd, updatePiPusle_amp=update, plot=plot)
-    return (W, Q, dataReceive, Id, Qd, piPulseAmp)
+
+    selData = psdp.PostSelectionData(Id, Qd, msmtInfoDict, [1, 0], plotGauFitting=0)
+    selMask = selData.mask_g_by_circle(sel_idx=0, plot=0)
+    I_vld, Q_vld = selData.sel_data(selMask, plot=0)
+    return (dataReceive, Id, Qd, piPulseAmp, I_vld, Q_vld)
 
 if __name__ == '__main__':
-    msmt = sciprotocol()
+
+    ampArray = np.linspace(0, 0.9, 100)
+
+    dataReceive, Id, Qd, piPulseAmp, I_vld, Q_vld = sciprotocol(ampArray)
+
+    N = len(I_vld)
+
+    tomo_axis_i = np.zeros(N)
+    initial_state_i = np.zeros(N)
+    amp_i = np.zeros(N)
+
+    for i in range(len(I_vld)):
+        I_i = I_vld[i]
+        Q_i = Q_vld[i]
+
+        tomo_axis_i = i % 3
+        initial_state_i = i % 9 // 3
+        amp_i = ampArray[i // 9]
+
+
+
+
+
+
+
 
